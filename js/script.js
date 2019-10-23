da2a8f9f687be9fefa2ebc5aa247c2e9b39a6b7d
Nasir2003 <elnasir03@gmail.com>
2019-10-23 21:59:39 +0000

    added a click handler, apended input to

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+$("button").click(function(){
+var ask = $("input").val()
+var fl = ask.substring(0, 1)
+var rl = ask.substring(1, rl.length)
+var finaloutput = ask.substring(rl)
+$(".output").append(ask);
+});
 
 
-  
-  
-  
-  
+
+
+
 });
