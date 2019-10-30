270498271998a240e7310323d23ca87cb50aa5e4
Nasir2003 <elnasir03@gmail.com>
2019-10-30 21:59:59 +0000

    editted piglatinizer

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $("button").click(function(){
 var ask = $("input").val()
 var fl = ask.substring(0, 1)
 var rl = ask.substring(1, ask.length)
-var finaloutput = ask.substring(rl)
-$(".output").append(ask);
+var finaloutput = (rl + fl + "ay")
+$(".output").append(finaloutput);
 });
 
 
